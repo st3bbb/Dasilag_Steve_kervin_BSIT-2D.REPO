f3c5c6fdb7b4e5af5017d898a8035cc4d53f9230
st3bbb <stevekervindasilag@gmail.com>
2025-04-21 15:08:17 +0800

    Update script.js

---
+++ Activities/Function - Activity/script.js
@@ -0,0 +1,15 @@
+function WhatShallIwear(temp){
+    if (temp < 60) {
+        console.log("Wear a jacket")
+    }
+    else if(temp < 70){
+        console.log("Wear a sweater")
+    }
+    else{
+        console.log("Wear a t-shirt")
+    }
+}
+
+WhatShallIwear(50)
+WhatShallIwear(80)
+WhatShallIwear(60)
